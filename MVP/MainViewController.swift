bc2d487e82aa91ca4615f818a950927fd643a116
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-22 16:37:41 +0300

    Update MainViewController.swift
    add binding

--- MVP/MainViewController.swift
+++ MVP/MainViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    var presenter: MainViewPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapButtonAction(_ sender: Any) {
+        self.presenter.showGreeting()
+    }
+}
+
+extension MainViewController: MainViewProtocol {
+    func setGreeting(greeting: String) {
+        self.greetingLabel.text = greeting
     }
 }
