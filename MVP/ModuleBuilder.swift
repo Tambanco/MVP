cfdf69b47a1d289ad017dbe626cb08ff216fd21e
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-22 17:01:20 +0300

    Update SceneDelegate
    with Module builder

--- MVP/ModuleBuilder.swift
+++ MVP/ModuleBuilder.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import UIKit
+
+protocol Builder {
+    static func createMainModule() -> UIViewController
+}
+
+class ModuleBuilder: Builder {
+    static func createMainModule() -> UIViewController {
+        let model = Person(firstName: "dfd", lastName: "dgfhgjh")
+        let view = MainViewController()
+        let presenter = MainPresenter(view: view, person: model)
+        view.presenter = presenter
+        return view
+    }
+}
