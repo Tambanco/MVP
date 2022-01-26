30f6f0b6d0e3488441d9bcc9db1a0cf5e63277ac
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-26 16:28:15 +0300

    Update with TableView
    add tableView into MainViewController

--- MVP/MainModule/View/MainViewController.swift
+++ MVP/MainModule/View/MainViewController.swift
@@ -10,22 +10,35 @@ import UIKit
 class MainViewController: UIViewController {
     
     // MARK: - IBOutlet
-    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var presenter: MainViewPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
     
-    // MARK: - @IBAction
-    @IBAction func didTapButtonAction(_ sender: Any) {
-        self.presenter.showGreeting()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Gafs"
+        cell.contentConfiguration = content
+        return cell
     }
 }
 
 extension MainViewController: MainViewProtocol {
     func setGreeting(greeting: String) {
-        self.greetingLabel.text = greeting
+        
     }
 }
