e0cc9e25d93edcdcf6c74375adbd9e6d536b9a06
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-26 18:36:10 +0300

    Create CollectionView

---
+++ MVP/MainModule/View/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  MVP
+//
+//  Created by tambanco 🥳 on 26.01.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let reuseId = "CollectionViewCell"
+    
+    // MARK: - Outlets
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+}
