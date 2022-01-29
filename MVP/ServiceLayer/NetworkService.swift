ffc81b95866e4dec9fadde0f7c617f8ac32309d0
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-29 12:27:57 +0300

    Update NetworkService.swift

--- MVP/ServiceLayer/NetworkService.swift
+++ MVP/ServiceLayer/NetworkService.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol NetworkServiceProtocol {
+    func getPasta(completion: @escaping (Result<[Pasta]?, Error>) -> Void)
+}
+
+class NetworkService: NetworkServiceProtocol {
+    func getPasta(completion: @escaping (Result<[Pasta]?, Error>) -> Void) {
+        let urlString = "https://raw.githubusercontent.com/Tambanco/PastaJSON/main/pasta.json"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            do {
+                let obj = try JSONDecoder().decode([Pasta].self, from: data!)
+                completion(.success(obj))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
