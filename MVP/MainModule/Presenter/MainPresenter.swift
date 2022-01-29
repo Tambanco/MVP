25ea937f7b0892eac89bda8c0ad53091553cea56
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-29 12:38:38 +0300

    Update MainPresenter.swift

--- MVP/MainModule/Presenter/MainPresenter.swift
+++ MVP/MainModule/Presenter/MainPresenter.swift
@@ -10,21 +10,38 @@ import Foundation
 // MARK: - Output protocol
 protocol MainViewProtocol: AnyObject {
     func success()
-    func failure()
+    func failure(error: Error)
 }
 
 // MARK: - Input protocol
 protocol MainViewPresenterProtocol: AnyObject {
     init(view: MainViewProtocol, networkService: NetworkServiceProtocol)
+    func getPasta()
+    var pasta: [Pasta]? { get set }
 }
 
 class MainPresenter: MainViewPresenterProtocol {
     
-    let view: MainViewProtocol
-    let networkService: NetworkServiceProtocol
+    let view: MainViewProtocol?
+    let networkService: NetworkServiceProtocol!
+    var pasta: [Pasta]?
+    
+    func getPasta() {
+        networkService.getPasta { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let pasta):
+                self.pasta = pasta
+                self.view?.success()
+            case .failure(let error):
+                self.view?.failure(error: error)
+            }
+        }
+    }
     
     required init(view: MainViewProtocol, networkService: NetworkServiceProtocol) {
         self.view = view
         self.networkService = networkService
+        getPasta()
     }
 }
