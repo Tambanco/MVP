f2b73d73e193254b29d80480eb935a64c072a15e
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-29 12:17:23 +0300

    add Pasta model

---
+++ MVP/MainModule/Model/Pasta.swift
@@ -0,0 +1,15 @@
+//
+//  Pasta.swift
+//  MVP
+//
+//  Created by tambanco 🥳 on 29.01.2022.
+//
+
+import Foundation
+
+struct Pasta: Decodable {
+    let id: Int
+    let name: String
+    let url: String
+    let text: String
+}
