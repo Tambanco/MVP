a4bee80254a0d5bece0dd707e32f1975225b0f94
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-29 11:36:06 +0300

    Connect simple empty cells

--- MVP/MainModule/View/MainViewController.swift
+++ MVP/MainModule/View/MainViewController.swift
@@ -12,11 +12,42 @@ class MainViewController: UIViewController {
     // MARK: - IBOutlet
     
     var presenter: MainViewPresenterProtocol!
+    var mainCollectionView: UICollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
+    var layout: UICollectionViewFlowLayout!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        layout = UICollectionViewFlowLayout()
+        mainCollectionView = UICollectionView(frame: CGRect(x: 0,
+                                                            y: 0,
+                                                            width: self.view.frame.width,
+                                                            height: self.view.frame.height), collectionViewLayout: layout)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 100, height: 100)
+        
+        mainCollectionView.dataSource = self
+        mainCollectionView.delegate = self
+        
+        view.addSubview(mainCollectionView)
+        
+        mainCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
+}
+
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .yellow
+        return cell 
+    }
+    
+    
 }
 
 
