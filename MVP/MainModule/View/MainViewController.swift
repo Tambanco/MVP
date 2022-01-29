2af3975bf82baff05a900b83caf0e4accc2fcdd8
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-29 10:39:54 +0300

    Update MainViewController.swift

--- MVP/MainModule/View/MainViewController.swift
+++ MVP/MainModule/View/MainViewController.swift
@@ -10,32 +10,16 @@ import UIKit
 class MainViewController: UIViewController {
     
     // MARK: - IBOutlet
-    @IBOutlet weak var tableView: UITableView!
     
     var presenter: MainViewPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.dataSource = self
-        tableView.delegate = self
+
     }
 }
 
-extension MainViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        var content = cell.defaultContentConfiguration()
-        content.text = "Gafs"
-        cell.contentConfiguration = content
-        return cell
-    }
-}
+
 
 extension MainViewController: MainViewProtocol {
     func setGreeting(greeting: String) {
